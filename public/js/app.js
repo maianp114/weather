edf63d68478ec31a1c6413bf0ec6c472a4e86d07
Mai666 <mai.anp114@gmail.com>
2020-07-16 21:32:48 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
 
   console.log(location);
-  fetch("http://localhost:3000/weather?address=" + location).then((res) => {
+  fetch("/weather?address=" + location).then((res) => {
     res.json().then((data) => {
       if (data.error) {
        return messageOne.textContent =data.error;
